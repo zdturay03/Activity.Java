8beca5418699025fa2384777928310daed609b44
zdturay03 <turayzandradanes@gmail.com>
2024-06-03 12:30:39 +0800

    Create HelloWorldApp.java

---
+++ HelloWorldApp.java
@@ -0,0 +1,11 @@
+class HelloWorldApp {
+  public static void main (String[] args) {
+
+    String hello ="Hi Mom";
+    System.out.printIn();
+  
+  }
+    public static String sayHi() {
+      return "Hi Mom!";
+    }
+}
